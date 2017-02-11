68095bd03d876a632e79664652231a73aaa8d567
DoubleRHR <rosener.riley@gmail.com>
2017-02-11 11:28:08 -0700

    Added text

--- Challenges/rvrs.py
+++ Challenges/rvrs.py
@@ -1,1 +1,7 @@
-
+#Reverses a given thing
+#Needs to be more elegant, but it'll do
+def rvrs():
+  str_input = input("Enter a string: ")
+  str_input = list(str_input[::-1])
+  str_input = ''.join(str_input)
+  print(str_input)
