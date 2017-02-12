8337d91429e2cdd143f1675fae7c5b887708f4b0
DoubleRHR <rosener.riley@gmail.com>
2017-02-12 14:48:24 -0700

    Added suggestions from the lovely folks at Stack

--- Challenges/pig_latin.py
+++ Challenges/pig_latin.py
@@ -6,3 +6,19 @@ def pig_latin(word):
   word.insert(len(word),first_letter)
   word.insert(len(word),'ay')
   print(''.join(word))
+  
+  #Suggested edits via StackOverflow -------------------
+  def pig_latin(word):
+    """Coverts to Pig Latin; moves first letter to end, and adds 'ay' to end
+    """
+    word = list(word)
+    first_letter = word[0]
+    del word[0]
+    word.append(first_letter)
+    word.append('ay')
+    print(''.join(word))
+    
+  def pig_latin(word):
+    print('%s%say'%(word[1:],word[0]))
+  #-----------------------------------------------------
+  
